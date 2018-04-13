ff2636e04c0bee99514f518b3ac8278af05234e7
Unknown <no.mercy.cs573@gmail.com>
2018-04-13 13:38:37 -0500

    GUI_FINAL

--- AES_gui.py
+++ AES_gui.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import *
 from tkinter import ttk
 from Encriptador import *
+import sys
+from tkinter import messagebox
 
 root = tk.Tk()
 
@@ -15,18 +17,36 @@ y_offset = 100
 root.geometry("%dx%d+%d+%d" % (width, height,x_offset,y_offset))
 
 
+def confirm(tam_bits,clave):
+    if(tam_bits == "16 bits" and len(clave) == 16):
+        return True
+    elif(tam_bits == "24 bits" and len(clave) == 24):
+        return True
+    elif(tam_bits == "32 bits" and len(clave) == 32):
+        return True
+    return False
+
 
 def encrypt():
     inputValue = textBox1.get("1.0","end-1c")
     key = textBox3.get("1.0","end-1c")
     Encrypter = Encriptador(key)
-    text_encrypted = Encrypter.Run(inputValue)
-    print(text_encrypted)
-    textBox2.insert(END,text_encrypted)
-    textBox2.config(state=DISABLED) 
-    file = open('encrypted_text.txt','w')
-    file.write(text_encrypted)
-    file.close()
+    if(confirm(combo.get(),key)):
+        text_encrypted = Encrypter.Run(inputValue)
+        print(text_encrypted)
+        textBox2.config(state=NORMAL)
+        textBox2.delete("1.0",END)
+        textBox2.insert(END,text_encrypted)
+        textBox2.config(state=DISABLED)
+        print(CheckVar1.get())
+        if(CheckVar1.get()):
+            file = open('encrypted_text.txt','w',encoding="utf-8")
+            file.write(text_encrypted)
+            file.close()
+    else:
+        result = messagebox.showerror("Confirm", "Clave erronea", icon='warning')
+        
+        
 
 
 frame = tk.Frame(root)
@@ -55,7 +75,7 @@ textBox2.place(x=320,y=100)
 
 
 #TextBox3
-textBox3 = tk.Text(frame,height = 1, width=17)
+textBox3 = tk.Text(frame,height = 1, width=55)
 textBox3.place(x=140,y=30)
 
 #ComboBox
