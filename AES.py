d7c59a5ceb123afa689db57867c6ed328ec727bc
texsmv <texs.mv@gmail.com>
2018-04-13 11:49:11 -0500

    funciones inversas

--- AES.py
+++ AES.py
@@ -24,17 +24,28 @@ class AES:
                     bloque.set_at(i, j, (sbox_inverse[int(hex_word[0], 16)][int(hex_word[1], 16)]))
         return bloque
 
-    def ShiftRows(self, bloque):
+    def ShiftRows(self, bloque, encrypt):
+		if encrypt == True:
+			enc = 1
+		else:
+			enc = -1
         for i in range(1, 4):
-            bloque.mat[i] = np.roll(bloque.mat[i], -i)
+            bloque.mat[i] = np.roll(bloque.mat[i], -i * enc)
         return bloque
 
-    def MixColumns(self,  bloque):
+    def MixColumns(self,  bloque, encrypt):
+		m_enc = [[2,3,1,1],[1,2,3,1],[1,1,2,3],[3,1,1,2]]
+		m_des = [[14,11,13,9],[9,14,11,13],[13,9,14,11],[11,13,9,14]]
+
+		if encrypt == True:
+			m = m_enc
+		else:
+			m = m_des
         for i in range(0, 4):
-            bloque.set_at(0, i, mult(bloque.at( 0, i), 2) ^ mult(bloque.at( 1, i), 3) ^ mult(bloque.at( 2, i), 1) ^ mult(bloque.at(3, i), 1))
-            bloque.set_at(1, i, mult(bloque.at( 0, i), 1) ^ mult(bloque.at( 1, i), 2) ^ mult(bloque.at( 2, i), 3) ^ mult(bloque.at(3, i), 1))
-            bloque.set_at(2, i, mult(bloque.at( 0, i), 1) ^ mult(bloque.at( 1, i), 1) ^ mult(bloque.at( 2, i), 2) ^ mult(bloque.at(3, i), 3))
-            bloque.set_at(3, i, mult(bloque.at( 0, i), 3) ^ mult(bloque.at( 1, i), 1) ^ mult(bloque.at( 2, i), 1) ^ mult(bloque.at(3, i), 2))
+            bloque.set_at(0, i, mult(bloque.at( 0, i), m[0][0]) ^ mult(bloque.at( 1, i), m[0][1]) ^ mult(bloque.at( 2, i), m[0][2]) ^ mult(bloque.at(3, i), m[0][3]))
+            bloque.set_at(1, i, mult(bloque.at( 0, i), m[1][0]) ^ mult(bloque.at( 1, i), m[1][1]) ^ mult(bloque.at( 2, i), m[1][2]) ^ mult(bloque.at(3, i), m[1][3]))
+            bloque.set_at(2, i, mult(bloque.at( 0, i), m[2][0]) ^ mult(bloque.at( 1, i), m[2][1]) ^ mult(bloque.at( 2, i), m[2][2]) ^ mult(bloque.at(3, i), m[2][3]))
+            bloque.set_at(3, i, mult(bloque.at( 0, i), m[3][0]) ^ mult(bloque.at( 1, i), m[3][0]) ^ mult(bloque.at( 2, i), m[3][2]) ^ mult(bloque.at(3, i), m[3][3]))
         return bloque
 
     def RunRounds(self, bloque, encryption_bool):
